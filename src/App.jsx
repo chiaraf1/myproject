87da2af7e0cc5f34070a7dd631f103349d6abee2
Chiara Finger <chiarafinger@MacBook-Air-de-Chiara.local>
2025-04-01 18:57:35 -0300

    Subiendo proyecto inicial

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import Contenido from "./Contenido"
+import { Header } from './Header'
+
+function App() {
+  
+  return (
+    <>
+  <Header />
+  <Contenido />
+    </>
+  )
+}
+
+export default App
---
+++ src/Contenido.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Contenido() {
+return (
+    <article className='project-curiosidades' >
+        
+
+        
+    </article>
+)
+}
+
+export default Contenido
