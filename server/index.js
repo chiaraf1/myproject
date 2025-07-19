adfb25f8f33392f121f9918bdabe659b456689e4
Chiara Finger <chiarafinger@MacBook-Air-de-Chiara.local>
2025-07-19 09:51:12 -0300

    vercel

--- server/index.js
+++ server/index.js
@@ -5,33 +5,35 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
-//Middlewares
+
+// Middleware CORS configurado
 const allowedOrigins = [
+  "https://myproject-d2cray4yv-chiaraf1s-projects.vercel.app",
   "https://myproject-git-master-chiaraf1s-projects.vercel.app",
   "http://localhost:5173"
 ];
-app.use(cors({
-  origin:allowedOrigins,
-  credentials:true 
-}));
-app.use(express.json());//pass json data
 
-//Rutas
+app.use(cors({
+  origin: allowedOrigins,
+  credentials: true
+}));
+
+app.use(express.json());
+
+// Rutas
 const userRoutes = require("./routes/usuarios");
 const postRoutes = require("./routes/postsRoutes");
 
-//Usar rutas
 app.use("/api", userRoutes);
 app.use("/api/posts", postRoutes);
 
-
+// Test
 app.get("/", (req, res) => {
   res.send("API CORRIENDO CORRECTAMENTE");
 });
 
-//PORT
+// PORT
 const PORT = process.env.PORT || 5001;
-//Start the server
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
